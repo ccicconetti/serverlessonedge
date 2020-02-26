f6252f34da16ccda233570622891d034ef3fd5ed
Claudio Cicconetti <ccicconetti@mbigroup.it>
2020-02-26 15:30:41 +0100

    Add Jenkins script

---
+++ jenkins/run_tests.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cd ${WORKSPACE}/build/debug
+
+echo "cleaning previous compilation"
+rm -rf * >& /dev/null
+
+../build.sh g++
+
+if [ $? -ne 0 ]; then
+  echo "cmake failed"
+  exit 1
+fi
+
+make -j5
+
+if [ $? -ne 0 ] ; then
+  echo "make failed"
+fi
+
+emulator/Test/testmain \
+  --gtest_shuffle \
+  --gtest_output=xml:${WORKSPACE}/testunit.xml
+
+exit $?
