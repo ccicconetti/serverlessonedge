c21cf91f9385e1a93d2b915fe711d1c75c8a8bf1
Claudio Cicconetti <ccicconetti@mbigroup.it>
2020-02-27 17:48:52 +0100

    Update Jenkins script to run all unit tests

--- jenkins/run_tests.sh
+++ jenkins/run_tests.sh
@@ -1,25 +1,61 @@
 #!/bin/bash
 
-cd ${WORKSPACE}/build/debug
-
-echo "cleaning previous compilation"
-rm -rf * >& /dev/null
-
-../build.sh g++
-
-if [ $? -ne 0 ]; then
-  echo "cmake failed"
-  exit 1
+if [ "$WORKSPACE" == "" ] ; then
+  WORKSPACE=$PWD
 fi
 
-make -j5
-
-if [ $? -ne 0 ] ; then
-  echo "make failed"
+if [ "$COMPILER" == "" ] ; then
+  COMPILER=g++
 fi
 
-Test/testmain \
-  --gtest_shuffle \
-  --gtest_output=xml:${WORKSPACE}/testunit.xml
+if [ "$CONCURRENCY" == "" ] ; then
+  CONCURRENCY=5
+fi
 
-exit $?
+MODULES=". etsimec etsimec/rest etsimec/rest/support"
+
+echo "WORKSPACE:   ${WORKSPACE}"
+echo "COMPILER:    ${COMPILER}"
+echo "CONCURRENCY: ${CONCURRENCY}"
+echo "MODULES:     ${MODULES}"
+
+ret=0
+counter=0
+for module in $MODULES ; do
+
+  echo "################################################################################"
+  echo "module: $module"
+
+  cd ${WORKSPACE}/$module/build/debug
+
+  echo "cleaning previous compilation"
+  rm -rf * >& /dev/null
+
+  ../build.sh ${COMPILER}
+
+  if [ $? -ne 0 ]; then
+    # exit immediately if cmake fails
+    echo "$module: cmake failed"
+    exit 1
+  fi
+
+  make -j${CONCURRENCY}
+
+  if [ $? -ne 0 ] ; then
+    # exit immediately if make fails
+    echo "$module: make failed"
+    exit 1
+  fi
+
+  counter=$(( counter + 1 ))
+  Test/testmain \
+    --gtest_shuffle \
+    --gtest_output=xml:${WORKSPACE}/testunit-$counter.xml
+
+  if [ $? -ne 0 ] ; then
+    # return error even if a single test fails, but continue running them
+    ret=1
+  fi
+done
+
+exit $ret
