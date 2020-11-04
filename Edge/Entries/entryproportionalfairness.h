dbe55e8c04d90fe2eacc597b945cfa8f22fb94af
leonardo <lossi771@gmail.com>
2020-11-04 11:17:25 +0100

    Added theAlpha, theBeta, theChrono for PF policy

--- Edge/Entries/entryproportionalfairness.h
+++ Edge/Entries/entryproportionalfairness.h
@@ -1,7 +1,9 @@
 #pragma once
 
+#include "Support/chrono.h"
 #include "entry.h"
 
+#include <cmath>
 #include <list>
 #include <string>
 
@@ -11,15 +13,18 @@ namespace entries {
 
 /**
  * Proportional Fairness Entry:
- * If the are multiple options available, the vhosen entry must be the one with
- * maximum prioritization coefficient (stored in the field weight in the Element
- * struct in Entry) Prioritization coefficients pc are computed as:
- * pc = T^alpha/R^beta
- * where:
+ * If the are multiple options available, the chosen entry must be the one with
+ * maximum prioritization coefficient (computed in the functional operator each
+ * time a destination for a given LambdaRequest must be scheduled)
+ *
+ * Prioritization coefficients PC are computed as: PC = T^alpha/R^beta where:
  * T = inverse of the last latency experienced serving a lambda request using
  * the destination
- * R = historical rate within a period determined by the PeriodicLocalOptimizer
- * (mean of the inverses of the latencies experienced within the period)
+ * R = ratio between theLambdaServedCount and the difference between the
+ * timestamp at scheduling time and theTimestamp associated with the
+ * destination (the latter can be the timestamp in which the destination has
+ * joined the system or the timestamp related to the last successful service of
+ * a LambdaRequest given by the destination)
  * alpha,beta = are parametere obtained by CLI to tune the fairness of the
  * scheduler:
  *    - alpha = 0, beta = 1 => Round Robin
@@ -29,7 +34,7 @@ namespace entries {
 class EntryProportionalFairness final : public Entry
 {
  public:
-  explicit EntryProportionalFairness();
+  explicit EntryProportionalFairness(double aAlpha, double aBeta);
 
  private:
   std::string operator()() override;
@@ -40,10 +45,15 @@ class EntryProportionalFairness final : public Entry
   void updateAddDest(const std::string& aDest, const float aWeight) override;
   void updateDelDest(const std::string& aDest, const float aWeight) override;
 
-  void update();
+  float computeWeight(float  experiencedLatency,
+                      int    theLambdaServedCount,
+                      double theDestinationTimestamp,
+                      double curTimestamp);
 
  private:
-  std::list<Element>::iterator theMaxElement;
+  support::Chrono theChrono;
+  const double    theAlpha;
+  const double    theBeta;
 };
 
 } // namespace entries
