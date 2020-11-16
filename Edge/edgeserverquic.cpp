b54b6967a5bc68b483aefc86126523d0efb108b8
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2020-11-16 17:41:22 +0100

    Add EdgeServerQuic skeleton

---
+++ Edge/edgeserverquic.cpp
@@ -0,0 +1,64 @@
+/*
+ ___ ___ __     __ ____________
+|   |   |  |   |__|__|__   ___/   Ubiquitout Internet @ IIT-CNR
+|   |   |  |  /__/  /  /  /    C++ edge computing libraries and tools
+|   |   |  |/__/  /   /  /  https://bitbucket.org/ccicconetti/edge_computing/
+|_______|__|__/__/   /__/
+
+Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+Copyright (c) 2018 Claudio Cicconetti <https://about.me/ccicconetti>
+
+Permission is hereby  granted, free of charge, to any  person obtaining a copy
+of this software and associated  documentation files (the "Software"), to deal
+in the Software  without restriction, including without  limitation the rights
+to  use, copy,  modify, merge,  publish, distribute,  sublicense, and/or  sell
+copies  of  the Software,  and  to  permit persons  to  whom  the Software  is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE  IS PROVIDED "AS  IS", WITHOUT WARRANTY  OF ANY KIND,  EXPRESS OR
+IMPLIED,  INCLUDING BUT  NOT  LIMITED TO  THE  WARRANTIES OF  MERCHANTABILITY,
+FITNESS FOR  A PARTICULAR PURPOSE AND  NONINFRINGEMENT. IN NO EVENT  SHALL THE
+AUTHORS  OR COPYRIGHT  HOLDERS  BE  LIABLE FOR  ANY  CLAIM,  DAMAGES OR  OTHER
+LIABILITY, WHETHER IN AN ACTION OF  CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE  OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include "edgeserverquic.h"
+
+#include <glog/logging.h>
+
+namespace uiiit {
+namespace edge {
+
+EdgeServerQuic::EdgeServerQuic(const std::string& aServerEndpoint,
+                       const size_t       aNumThreads)
+    : theServerEndpoint(aServerEndpoint)
+    , theNumThreads(aNumThreads) {
+  if (aNumThreads == 0) {
+    throw std::runtime_error("Cannot spawn 0 threads");
+  }
+}
+
+void EdgeServerQuic::run() {
+  // XXX
+  
+  init();
+}
+
+void EdgeServerQuic::wait() {
+  // XXX
+}
+
+EdgeServerQuic::~EdgeServerQuic() {
+}
+
+std::set<std::thread::id> EdgeServerQuic::threadIds() const {
+  return std::set<std::thread::id>();
+}
+
+} // namespace edge
+} // namespace uiiit
