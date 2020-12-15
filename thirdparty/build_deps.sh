595c446288d6c957c46989d4cedb5da605390d30
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2020-12-16 00:57:35 +0100

    Use shared libraries for proxygen

--- thirdparty/build_deps.sh
+++ thirdparty/build_deps.sh
@@ -7,6 +7,7 @@ if [ ! -d proxygen ] ; then
   pushd proxygen
   git checkout v2020.12.07.00 #v2020.10.12.00
   pushd proxygen
+  patch -p0 < ../../patches/proxygen
   ./build.sh -q >& build.log
   RET=$?
   popd
@@ -17,3 +18,24 @@ if [ ! -d proxygen ] ; then
     exit 1
   fi
 fi
+
+echo "**********************************************************************"
+echo "CREATING SYMLINKS"
+rm -rf include libs 2> /dev/null
+mkdir include libs 2> /dev/null
+pushd libs
+ln -s $(find ../proxygen/proxygen/_build_shared/ -name "*.so*" | grep -v '/deps/lib/') .
+popd
+
+pushd proxygen
+tar cf ../tmp1.$$.tar $(find proxygen -name *.h | grep -v _build)
+pushd proxygen/_build_shared/generated
+tar cf ../../../../tmp2.$$.tar $(find . -name *.h)
+popd
+popd
+pushd include
+ln -s ../proxygen/proxygen/_build_shared/deps/include/* .
+tar xf ../tmp1.$$.tar
+tar xf ../tmp2.$$.tar
+popd
+rm -f tmp[12].$$.tar
