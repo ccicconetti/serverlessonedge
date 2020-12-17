3ed822286cef7fda036601245b4364bd3775c241
Claudio Cicconetti <ccicconetti@mbigroup.it>
2020-12-17 18:45:07 +0100

    Add statesim::Network skeleton

---
+++ Test/teststatesim.cpp
@@ -0,0 +1,80 @@
+/*
+ ___ ___ __     __ ____________
+|   |   |  |   |__|__|__   ___/   Ubiquitout Internet @ IIT-CNR
+|   |   |  |  /__/  /  /  /    C++ edge computing libraries and tools
+|   |   |  |/__/  /   /  /  https://bitbucket.org/ccicconetti/edge_computing/
+|_______|__|__/__/   /__/
+
+Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+Copyright (c) 2018 Claudio Cicconetti <https://about.me/ccicconetti>
+
+Permission is hereby  granted, free of charge, to any  person obtaining a copy
+of this software and associated  documentation files (the "Software"), to deal
+in the Software  without restriction, including without  limitation the rights
+to  use, copy,  modify, merge,  publish, distribute,  sublicense, and/or  sell
+copies  of  the Software,  and  to  permit persons  to  whom  the Software  is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE  IS PROVIDED "AS  IS", WITHOUT WARRANTY  OF ANY KIND,  EXPRESS OR
+IMPLIED,  INCLUDING BUT  NOT  LIMITED TO  THE  WARRANTIES OF  MERCHANTABILITY,
+FITNESS FOR  A PARTICULAR PURPOSE AND  NONINFRINGEMENT. IN NO EVENT  SHALL THE
+AUTHORS  OR COPYRIGHT  HOLDERS  BE  LIABLE FOR  ANY  CLAIM,  DAMAGES OR  OTHER
+LIABILITY, WHETHER IN AN ACTION OF  CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE  OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include "StateSim/network.h"
+
+#include "gtest/gtest.h"
+
+#include <boost/filesystem.hpp>
+
+#include "Test/Data/datastatesim.h"
+
+namespace uiiit {
+namespace statesim {
+
+struct TestStateSim : public ::testing::Test {
+  TestStateSim()
+      : theTestDir("TO_REMOVE_DIR") {
+  }
+
+  void SetUp() {
+    boost::filesystem::remove_all(theTestDir);
+    boost::filesystem::create_directories(theTestDir);
+  }
+
+  void TearDown() {
+    boost::filesystem::remove_all(theTestDir);
+  }
+
+  bool prepareNetworkFiles() {
+    std::ofstream myEdges((theTestDir / "edges").string());
+    myEdges << theEdges;
+
+    std::ofstream myLinks((theTestDir / "links").string());
+    myLinks << theLinks;
+
+    std::ofstream myNodes((theTestDir / "nodes").string());
+    myNodes << theNodes;
+
+    std::ofstream myGraph((theTestDir / "graph").string());
+    myGraph << theGraph;
+
+    return static_cast<bool>(myEdges) and static_cast<bool>(myLinks) and
+           static_cast<bool>(myNodes) and static_cast<bool>(myGraph);
+  }
+
+  const boost::filesystem::path theTestDir;
+};
+
+TEST_F(TestStateSim, test_network) {
+  ASSERT_TRUE(prepareNetworkFiles());
+}
+
+} // namespace statesim
+} // namespace uiiit
