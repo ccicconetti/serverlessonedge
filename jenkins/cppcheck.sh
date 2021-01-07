13663112875720f4e3ffb7fb34d53a50c33b38b8
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2021-01-07 14:36:42 +0100

    Add cppcheck script

---
+++ jenkins/cppcheck.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+CPPCHECK=/usr/local/bin/cppcheck
+OPTIONS="--library=gnu --library=posix \
+         --platform=unix64 --std=c++17 \
+         --suppressions-list=cppcheck.suppression \
+         --enable=all \
+         --xml \
+         --xml-version=2 \
+         -I../emulator \
+         -I../build/debug/emulator/proto-src \
+         --suppress=syntaxError \
+         -j 8 \
+         --force"
+         
+#-I../build/debug/googletest-src/googletest/include \
+
+${CPPCHECK} ${OPTIONS} \
+  ../Edge \
+  ../Executables \
+  ../Factory \
+  ../OpenCV \
+  ../Rpc \
+  ../Simulation
+  ../Test \
+  ../etsimec/EtsiMec \
+  ../etsimec/Executables \
+  ../etsimec/OpenWhisk \
+  ../etsimec/Test \
+  ../etsimec/rest/Rest \
+  ../etsimec/rest/Test \
+  ../etsimec/rest/support/RpcSupport \
+  ../etsimec/rest/support/Support \
+  ../etsimec/rest/support/Test \
+  2>report.xml
