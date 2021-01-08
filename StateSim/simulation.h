cc431ae26ebeb3b64ddb108ea9995d2d12d418e6
Claudio Cicconetti <ccicconetti@mbigroup.it>
2021-01-08 11:35:06 +0100

    Add statesim::Simulation skeleton

---
+++ StateSim/simulation.h
@@ -0,0 +1,69 @@
+/*
+ ___ ___ __     __ ____________
+|   |   |  |   |__|__|__   ___/   Ubiquitout Internet @ IIT-CNR
+|   |   |  |  /__/  /  /  /    C++ edge computing libraries and tools
+|   |   |  |/__/  /   /  /  https://bitbucket.org/ccicconetti/edge_computing/
+|_______|__|__/__/   /__/
+
+Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+Copyright (c) 2018 Claudio Cicconetti <https://about.me/ccicconetti>
+
+Permission is hereby  granted, free of charge, to any  person obtaining a copy
+of this software and associated  documentation files (the "Software"), to deal
+in the Software  without restriction, including without  limitation the rights
+to  use, copy,  modify, merge,  publish, distribute,  sublicense, and/or  sell
+copies  of  the Software,  and  to  permit persons  to  whom  the Software  is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE  IS PROVIDED "AS  IS", WITHOUT WARRANTY  OF ANY KIND,  EXPRESS OR
+IMPLIED,  INCLUDING BUT  NOT  LIMITED TO  THE  WARRANTIES OF  MERCHANTABILITY,
+FITNESS FOR  A PARTICULAR PURPOSE AND  NONINFRINGEMENT. IN NO EVENT  SHALL THE
+AUTHORS  OR COPYRIGHT  HOLDERS  BE  LIABLE FOR  ANY  CLAIM,  DAMAGES OR  OTHER
+LIABILITY, WHETHER IN AN ACTION OF  CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE  OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#pragma once
+
+#include "StateSim/scenario.h"
+#include "Support/macros.h"
+
+namespace uiiit {
+namespace statesim {
+
+class Simulation final
+{
+  NONCOPYABLE_NONMOVABLE(Simulation);
+
+ public:
+  struct Conf {
+    //! File containing the info about nodes
+    const std::string& theNodesPath;
+    //! File containing the info about links
+    const std::string theLinksPath;
+    //! File containing the info about edges
+    const std::string theEdgesPath;
+    //! File containing the info about tasks
+    const std::string theTasksPath;
+
+    std::string toString() const;
+  };
+
+  //! Create a simulation environment.
+  explicit Simulation(const size_t aNumThreads);
+
+  //! Run a batch of simulations.
+  void run(const Conf&  aConf,
+           const size_t aStartingSeed,
+           const size_t aNumReplications);
+
+ private:
+  const size_t theNumThreads;
+};
+
+} // namespace statesim
+} // namespace uiiit
