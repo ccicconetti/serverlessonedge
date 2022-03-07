6578ff42d2dab8ba8d5dc66fa38c8141cf069fb1
Claudio Cicconetti <cicconetti@gmail.com>
2022-03-07 17:20:26 +0100

    Simulation 011: update scripts

--- simulations/011_LambdaMuSim_dynamic/analyze.sh
+++ simulations/011_LambdaMuSim_dynamic/analyze.sh
@@ -20,23 +20,22 @@ if [ ! -d "post" ] ; then
   mkdir post 2> /dev/null
 fi
 
-mus="25 50 75"
-alphas="0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9"
+apps="50 100 200"
+epochs="3.6e4 3.6e5 3.6e6"
 
 infile=data/urban0.csv
 
 columns=(11 12 15 16 17)
 names=("num-containers" "tot-capacity" "lambda-cost" "mu-cost" "mu-cloud")
 
-for m in $mus ; do
+for a in $apps ; do
   for i in ${!columns[@]}; do
-    outmangle=${names[$i]}-$m
+    outmangle=${names[$i]}-$a
     echo "$outmangle"
     outfile=post/$outmangle.dat
     rm -f $outfile 2> /dev/null
-    for a in $alphas ; do
-      inmangle=out-$f-$m
-      fingerprint=",2.000000,50,$m,$a""00000,0.500000,1,"
+    for e in $epochs ; do
+      fingerprint="2.000000,$e,50,$a,10,10,0.500000,0.500000,1"
       value=$(grep $fingerprint $infile | $percentile_script --delimiter , --column ${columns[$i]} --mean | cut -f 1,3 -d ' ')
       echo $a $value >> $outfile
     done
--- simulations/011_LambdaMuSim_dynamic/run.sh
+++ simulations/011_LambdaMuSim_dynamic/run.sh
@@ -4,31 +4,35 @@ if [ "$DRY" == "" ] ; then
   mkdir data 2> /dev/null
 fi
 
-epochs="3.6e6"
+apps="50 100 200"
+epochs="60e3 300e3 600e3 1200e3 1800e3 3600e3"
 
 for e in $epochs ; do
-  cmd="./lambdamusim \
-        --type dynamic \
-        --nodes-path network/urban_sensing.0.nodes \
-        --links-path network/urban_sensing.0.links \
-        --edges-path network/urban_sensing.0.edges \
-        --cloud-distance-factor 2 \
-        --duration 8.64e7 \
-        --warm-up $e \
-        --epoch $e \
-        --min-periods 50 \
-        --avg-apps 100 \
-        --alpha 0.5 \
-        --beta 0.5 \
-        --lambda-request 1 \
-        --out-file data/urban0.csv \
-        --append \
-        --seed-starting 1 \
-        --num-replications 64"
+  for a in $apps ; do
+    cmd="./lambdamusim \
+          --type dynamic \
+          --nodes-path network/urban_sensing.0.nodes \
+          --links-path network/urban_sensing.0.links \
+          --edges-path network/urban_sensing.0.edges \
+          --cloud-distance-factor 2 \
+          --apps-path azurefunctions-accesses-2020-timestamps \
+          --duration 8.64e7 \
+          --warm-up $e \
+          --epoch $e \
+          --min-periods 50 \
+          --avg-apps $a \
+          --alpha 0.5 \
+          --beta 0.5 \
+          --lambda-request 1 \
+          --out-file data/urban0.csv \
+          --append \
+          --seed-starting 1 \
+          --num-replications 6400"
 
-  if [ "$DRY" == "" ] ; then
-    $cmd
-  else
-    echo $cmd
-  fi
+    if [ "$DRY" == "" ] ; then
+      $cmd
+    else
+      echo $cmd
+    fi
+  done
 done
