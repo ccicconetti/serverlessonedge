a540eb0fce6505e7029f98230677783b5a2402e0
Claudio Cicconetti <cicconetti@gmail.com>
2022-08-24 12:41:16 +0200

    Add client to TestMvfst

--- Test/testmvfst.cpp
+++ Test/testmvfst.cpp
@@ -41,8 +41,20 @@ SOFTWARE.
 
 #include <quic/QuicConstants.h>
 #include <quic/QuicException.h>
+#include <quic/api/QuicSocket.h>
+#include <quic/client/QuicClientTransport.h>
 #include <quic/codec/Types.h>
+#include <quic/common/BufUtil.h>
+#include <quic/fizz/client/handshake/FizzClientQuicHandshakeContext.h>
 #include <quic/state/QuicTransportStatsCallback.h>
+
+#include <fizz/protocol/CertificateVerifier.h>
+#include <folly/fibers/Baton.h>
+#include <folly/io/async/ScopedEventBaseThread.h>
+
+#include <iostream>
+#include <string>
+#include <thread>
 
 namespace uiiit {
 namespace mvfst {
@@ -291,6 +303,212 @@ class LogQuicStats : public quic::QuicTransportStatsCallback
 
  private:
   std::string prefix_;
+};
+
+class TestCertificateVerifier : public fizz::CertificateVerifier
+{
+ public:
+  ~TestCertificateVerifier() override = default;
+
+  std::shared_ptr<const folly::AsyncTransportCertificate>
+  verify(const std::vector<std::shared_ptr<const fizz::PeerCert>>& certs)
+      const override {
+    return certs.front();
+  }
+
+  [[nodiscard]] std::vector<fizz::Extension>
+  getCertificateRequestExtensions() const override {
+    return std::vector<fizz::Extension>();
+  }
+};
+
+class EchoClient : public quic::QuicSocket::ConnectionSetupCallback,
+                   public quic::QuicSocket::ConnectionCallback,
+                   public quic::QuicSocket::ReadCallback,
+                   public quic::QuicSocket::WriteCallback,
+                   public quic::QuicSocket::DatagramCallback
+{
+ public:
+  EchoClient(const std::string& host, uint16_t port)
+      : host_(host)
+      , port_(port) {
+    // noop
+  }
+
+  void readAvailable(quic::StreamId streamId) noexcept override {
+    auto readData = quicClient_->read(streamId, 0);
+    if (readData.hasError()) {
+      LOG(ERROR) << "EchoClient failed read from stream=" << streamId
+                 << ", error=" << (uint32_t)readData.error();
+    }
+    auto copy = readData->first->clone();
+    if (recvOffsets_.find(streamId) == recvOffsets_.end()) {
+      recvOffsets_[streamId] = copy->length();
+    } else {
+      recvOffsets_[streamId] += copy->length();
+    }
+    LOG(INFO) << "Client received data=" << copy->moveToFbString().toStdString()
+              << " on stream=" << streamId;
+  }
+
+  void readError(quic::StreamId  streamId,
+                 quic::QuicError error) noexcept override {
+    LOG(ERROR) << "EchoClient failed read from stream=" << streamId
+               << ", error=" << toString(error);
+    // A read error only terminates the ingress portion of the stream state.
+    // Your application should probably terminate the egress portion via
+    // resetStream
+  }
+
+  void onNewBidirectionalStream(quic::StreamId id) noexcept override {
+    LOG(INFO) << "EchoClient: new bidirectional stream=" << id;
+    quicClient_->setReadCallback(id, this);
+  }
+
+  void onNewUnidirectionalStream(quic::StreamId id) noexcept override {
+    LOG(INFO) << "EchoClient: new unidirectional stream=" << id;
+    quicClient_->setReadCallback(id, this);
+  }
+
+  void onStopSending(quic::StreamId id,
+                     quic::ApplicationErrorCode /*error*/) noexcept override {
+    VLOG(10) << "EchoClient got StopSending stream id=" << id;
+  }
+
+  void onConnectionEnd() noexcept override {
+    LOG(INFO) << "EchoClient connection end";
+  }
+
+  void onConnectionSetupError(quic::QuicError error) noexcept override {
+    onConnectionError(std::move(error));
+  }
+
+  void onConnectionError(quic::QuicError error) noexcept override {
+    LOG(ERROR) << "EchoClient error: " << toString(error.code)
+               << "; errStr=" << error.message;
+    startDone_.post();
+  }
+
+  void onTransportReady() noexcept override {
+    startDone_.post();
+  }
+
+  void onStreamWriteReady(quic::StreamId id,
+                          uint64_t       maxToSend) noexcept override {
+    LOG(INFO) << "EchoClient socket is write ready with maxToSend="
+              << maxToSend;
+    sendMessage(id, pendingOutput_[id]);
+  }
+
+  void onStreamWriteError(quic::StreamId  id,
+                          quic::QuicError error) noexcept override {
+    LOG(ERROR) << "EchoClient write error with stream=" << id
+               << " error=" << toString(error);
+  }
+
+  void onDatagramsAvailable() noexcept override {
+    auto res = quicClient_->readDatagrams();
+    if (res.hasError()) {
+      LOG(ERROR) << "EchoClient failed reading datagrams; error="
+                 << res.error();
+      return;
+    }
+    for (const auto& datagram : *res) {
+      LOG(INFO) << "Client received datagram ="
+                << datagram.bufQueue()
+                       .front()
+                       ->cloneCoalesced()
+                       ->moveToFbString()
+                       .toStdString();
+    }
+  }
+
+  void start(std::string token) {
+    folly::ScopedEventBaseThread networkThread("EchoClientThread");
+    auto                         evb = networkThread.getEventBase();
+    folly::SocketAddress         addr(host_.c_str(), port_);
+
+    evb->runInEventBaseThreadAndWait([&] {
+      auto sock = std::make_unique<folly::AsyncUDPSocket>(evb);
+      auto fizzClientContext =
+          quic::FizzClientQuicHandshakeContext::Builder()
+              .setCertificateVerifier(
+                  std::make_unique<TestCertificateVerifier>())
+              .build();
+      quicClient_ = std::make_shared<quic::QuicClientTransport>(
+          evb, std::move(sock), std::move(fizzClientContext));
+      quicClient_->setHostname("echo.com");
+      quicClient_->addNewPeerAddress(addr);
+      if (!token.empty()) {
+        quicClient_->setNewToken(token);
+      }
+
+      quic::TransportSettings settings;
+      settings.datagramConfig.enabled      = false;
+      settings.selfActiveConnectionIdLimit = 10; // max active concurrent conns
+      settings.disableMigration            = true;
+      quicClient_->setTransportSettings(settings);
+
+      quicClient_->setTransportStatsCallback(
+          std::make_shared<LogQuicStats>("client"));
+
+      LOG(INFO) << "EchoClient connecting to " << addr.describe();
+      quicClient_->start(this, this);
+    });
+
+    startDone_.wait();
+
+    std::string message;
+    bool        closed = false;
+    auto        client = quicClient_;
+
+    auto sendMessageInStream = [&]() {
+      if (message == "/close") {
+        quicClient_->close(folly::none);
+        closed = true;
+        return;
+      }
+
+      // create new stream for each message
+      auto streamId = client->createBidirectionalStream().value();
+      client->setReadCallback(streamId, this);
+      pendingOutput_[streamId].append(folly::IOBuf::copyBuffer(message));
+      sendMessage(streamId, pendingOutput_[streamId]);
+    };
+
+    // loop until Ctrl+D
+    while (!closed && std::getline(std::cin, message)) {
+      if (message.empty()) {
+        continue;
+      }
+      evb->runInEventBaseThreadAndWait([=] { sendMessageInStream(); });
+    }
+    LOG(INFO) << "EchoClient stopping client";
+  }
+
+  ~EchoClient() override = default;
+
+ private:
+  void sendMessage(quic::StreamId id, quic::BufQueue& data) {
+    auto message = data.move();
+    auto res     = quicClient_->writeChain(id, message->clone(), true);
+    if (res.hasError()) {
+      LOG(ERROR) << "EchoClient writeChain error=" << uint32_t(res.error());
+    } else {
+      auto str = message->moveToFbString().toStdString();
+      LOG(INFO) << "EchoClient wrote \"" << str << "\""
+                << ", len=" << str.size() << " on stream=" << id;
+      // sent whole message
+      pendingOutput_.erase(id);
+    }
+  }
+
+  std::string                                host_;
+  uint16_t                                   port_;
+  std::shared_ptr<quic::QuicClientTransport> quicClient_;
+  std::map<quic::StreamId, quic::BufQueue>   pendingOutput_;
+  std::map<quic::StreamId, uint64_t>         recvOffsets_;
+  folly::fibers::Baton                       startDone_;
 };
 
 class LogQuicStatsFactory : public quic::QuicTransportStatsCallbackFactory
