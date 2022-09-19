8d06bb6756979db4cde410c8068fb9343c197513
Claudio Cicconetti <cicconetti@gmail.com>
2022-09-19 17:28:43 +0200

    Update create_cert.sh
    Now the env variable SERVER determines the domain

--- utils/create_cert.sh
+++ utils/create_cert.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-# from:
+# based on:
 # https://github.com/grpc/grpc/issues/9593#issuecomment-277946137
+
+if [ "$SERVER" == "" ] ; then
+  SERVER=localhost
+fi
+
+echo "generating certificates for server: $SERVER"
 
 # Generate valid CA
 openssl genrsa -passout pass:1234 -des3 -out ca.key 4096
-openssl req -passin pass:1234 -new -x509 -days 365 -key ca.key -out ca.crt -subj  "/C=SP/ST=Spain/L=Valdepenias/O=Test/OU=Test/CN=Root CA"
+openssl req -passin pass:1234 -new -x509 -days 365 -key ca.key -out ca.crt -subj  "/C=SP/ST=Italy/L=Pisa/O=CNR-IIT-UI/OU=Test/CN=Root CA"
 
 # Generate valid Server Key/Cert
 openssl genrsa -passout pass:1234 -des3 -out server.key 4096
-openssl req -passin pass:1234 -new -key server.key -out server.csr -subj  "/C=SP/ST=Spain/L=Valdepenias/O=Test/OU=Server/CN=localhost"
+openssl req -passin pass:1234 -new -key server.key -out server.csr -subj  "/C=SP/ST=Italy/L=Pisa/O=CNR-IIT-UI/OU=Server/CN=$SERVER"
 openssl x509 -req -passin pass:1234 -days 365 -in server.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out server.crt
 
 # Remove passphrase from the Server Key
@@ -17,7 +23,7 @@ openssl rsa -passin pass:1234 -in server.key -out server.key
 
 # Generate valid Client Key/Cert
 openssl genrsa -passout pass:1234 -des3 -out client.key 4096
-openssl req -passin pass:1234 -new -key client.key -out client.csr -subj  "/C=SP/ST=Spain/L=Valdepenias/O=Test/OU=Client/CN=localhost"
+openssl req -passin pass:1234 -new -key client.key -out client.csr -subj  "/C=SP/ST=Italy/L=Pisa/O=CNR-IIT-UI/OU=Client/CN=$SERVER"
 openssl x509 -passin pass:1234 -req -days 365 -in client.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out client.crt
 
 # Remove passphrase from Client Key
