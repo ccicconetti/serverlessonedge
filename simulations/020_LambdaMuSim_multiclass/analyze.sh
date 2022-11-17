d942c63c2f342407313296754c17ff897995ed4a
Claudio Cicconetti <cicconetti@gmail.com>
2022-11-17 11:44:48 +0100

    Simulation 020: update post script and add plots

--- simulations/020_LambdaMuSim_multiclass/analyze.sh
+++ simulations/020_LambdaMuSim_multiclass/analyze.sh
@@ -16,7 +16,7 @@ infile=data/020-urban0.csv
 columns=(15 16 19 20 21 22 23)
 names=("num-containers" "tot-capacity" "lambda-cost" "lambda-service-cloud" "mu-cost" "mu-cloud" "mu-service-cloud")
 
-numapps="10 20 50 100"
+numapps="20 40 60 80 100 120 140 160 180 200"
 algos="random greedy proposed"
 
 for a in $algos ; do
@@ -32,7 +32,7 @@ for a in $algos ; do
     outfile=post/$outmangle.dat
     rm -f $outfile 2> /dev/null
     for n in $numapps ; do
-      fingerprint="2.000000,0.000000,100.000000,60000.000000,50,10,$((n/2)),$((n/2)),0.500000,1.000000,classes;0.5;1;100;1;0.5;10;1;10,$algo_fp"
+      fingerprint="2.000000,0.000000,10.000000,60000.000000,50,10,$((n*3/4)),$((n/4)),0.500000,1.000000,classes;0.5;1;100;10;0.5;10;1;0,$algo_fp"
       value=$(grep $fingerprint $infile | $percentile_script --delimiter , --column ${columns[$i]} --mean | cut -f 1,3 -d ' ')
       echo $n $value >> $outfile
     done
