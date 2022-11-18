dd435962b251ab5b4e3afe3eb5c4aba37823e349
Claudio Cicconetti <cicconetti@gmail.com>
2022-11-18 10:14:15 +0100

    Simulation 021: add post script

---
+++ simulations/021_LambdaMuSim_multisize/analyze.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+if [ ! -d "data" ] ; then
+  echo "no 'data' directory, bailing out..."
+  exit 1
+fi
+
+if [ ! -d "post" ] ; then
+  mkdir post 2> /dev/null
+fi
+
+. ../../scripts/download-percentile.sh
+
+infile=data/021-urban0.csv
+
+columns=(15 16 19 20 21 22 23)
+names=("num-containers" "tot-capacity" "lambda-cost" "lambda-service-cloud" "mu-cost" "mu-cloud" "mu-service-cloud")
+
+numapps="50 100 150 200"
+algos="random greedy proposed"
+exchange_sizes="0 20 40 60 80 100"
+storage_sizes="0 2 4 6 8 10"
+
+for n in $numapps ; do
+for a in $algos ; do
+
+  algo_fp=$a,$a
+  if [ "$a" == "proposed" ] ; then
+    algo_fp=hungarian,mcfp
+  fi
+
+  for i in ${!columns[@]} ; do
+    outmangle=${names[$i]}-$n-$a
+    echo "$outmangle"
+    outfile=post/$outmangle.dat
+    rm -f $outfile 2> /dev/null
+    for e in $exchange_sizes ; do
+    for s in $storage_sizes ; do
+      fingerprint="2.000000,0.000000,10.000000,60000.000000,50,10,$((n*3/4)),$((n/4)),0.500000,1.000000,classes;0.5;5;100;10;0.5;5;$e;$s,$algo_fp"
+      value=$(grep $fingerprint $infile | $percentile_script --delimiter , --column ${columns[$i]} --mean | cut -f 1,3 -d ' ')
+      echo $e $s $value >> $outfile
+    done
+    done
+  done
+
+done
+done
