ecb0a0e7f67ec613f80acc8d36a8539725fe146b
Claudio Cicconetti <cicconetti@gmail.com>
2022-11-18 10:14:10 +0100

    Simulation 021: fix download artifacts script

--- simulations/021_LambdaMuSim_multisize/download-raw.sh
+++ simulations/021_LambdaMuSim_multisize/download-raw.sh
@@ -2,9 +2,9 @@
 
 mkdir data 2> /dev/null
 
-if [ -r data/012-urban0.csv ] ; then
+if [ -r data/021-urban0.csv ] ; then
   echo "raw data file already present: cowardly refusing to proceed"
   exit 1
 else
-  wget http://turig.iit.cnr.it/~claudio/public/012-urban0.csv.gz -O- | gzip -dc > data/012-urban0.csv
+  wget http://turig.iit.cnr.it/~claudio/public/021-urban0.csv.gz -O- | gzip -dc > data/021-urban0.csv
 fi
